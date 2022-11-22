51a582ba2d22d1e9163d6b674b62259ee9d2a022
Vik Sluijter <vik.sluijter@student.hogent.be>
2022-11-22 09:30:55 +0100

    Kunnen Inloggen met Spotify

--- src/login.jsx
+++ src/login.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "./Login.css"
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=ba4fb50e2f9b4d0d89efd7615c2a2d07&response_type=code&redirect_uri=https://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=ba4fb50e2f9b4d0d89efd7615c2a2d07&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function login() {
   return (
     <div>
-      <button><a href={AUTH_URL}>Login with spotify</a></button>
+      <button className='button'><a href={AUTH_URL}>Login with spotify</a></button>
     </div>
   )
 }
